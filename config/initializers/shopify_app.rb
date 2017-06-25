5e7ddf54e64d96a4da0fc7185feeab29febc3d28
Lucas Amorim <amorimluc@gmail.com>
2017-06-24 21:46:21 -0300

    Made Shopify API keys config variables

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "b8aa87575bc271fc4d37c45144bffcec"
-  config.secret = "fbd1e309106755cfd034ee6d21c44a91"
+  config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
+  config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end

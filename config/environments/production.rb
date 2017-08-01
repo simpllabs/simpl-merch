2dab990229649e572426e37e7683aeeb332c2b3c
Lucas Amorim <amorimluc@gmail.com>
2017-08-01 03:16:30 -0300

    Trying to fix mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,12 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "localhost",
+    :port => 25,
+    :domain => "mail.rocketees.com",
+  }
 
   #ActionMailer::Base.smtp_settings = {
   #  :address        => 'smtp.gmail.com',

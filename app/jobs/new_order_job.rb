3f53d463ad153bd385a76843af68f6b0f7e3ab13
Lucas Amorim <amorimluc@gmail.com>
2018-01-01 12:33:29 -0200

    fixes

--- app/jobs/new_order_job.rb
+++ app/jobs/new_order_job.rb
@@ -57,7 +57,7 @@ class NewOrderJob < ProgressJob::Base
                     order.city = order_params.shipping_address.city
                     order.zip = order_params.shipping_address.zip
                     order.province = order_params.shipping_address.province
-                    order.country = order_params.shipping_address.country
+                    order.country = order_params.shipping_address.country_code
                     order.front_design = tee.tee_front_url
                     order.back_design = tee.tee_back_url
                     order.front_ref = tee.tee_front_ref
@@ -103,7 +103,7 @@ class NewOrderJob < ProgressJob::Base
                     order.city = @params[:shipping_address][:city]
                     order.zip = @params[:shipping_address][:zip]
                     order.province = @params[:shipping_address][:province]
-                    order.country = @params[:shipping_address][:country]
+                    order.country = @params[:shipping_address][:country_code]
                     order.front_design = tee.tee_front_url
                     order.back_design = tee.tee_back_url
                     order.front_ref = tee.tee_front_ref

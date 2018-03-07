82511c1cfe1143cd3447f01088be558846e720db
Lucas Amorim <amorimluc@gmail.com>
2018-03-07 12:13:00 -0300

    Use my Zapier system

--- app/mailers/per_order_mailer.rb
+++ app/mailers/per_order_mailer.rb
@@ -1,8 +1,9 @@
 class PerOrderMailer < ApplicationMailer
 	default from: 'amorimluc@gmail.com'
+	#new will be l4ddigct@robot.zapier.com
 
 	def order_email(order)
 		@order = order
-		mail(to: "rock3t33s@robot.zapier.com", subject: 'Rocketees: New Order')
+		mail(to: "l4ddigct@robot.zapier.com", subject: 'Rocketees: New Order')
 	end
 end

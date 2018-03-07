b2132e1014aaf4f40a5dabba31fb02c982c73c71
Lucas Amorim <amorimluc@gmail.com>
2018-03-07 16:08:16 -0300

    Update Zapier system

--- app/mailers/per_order_mailer.rb
+++ app/mailers/per_order_mailer.rb
@@ -1,9 +1,9 @@
 class PerOrderMailer < ApplicationMailer
 	default from: 'amorimluc@gmail.com'
-	#new will be l4ddigct@robot.zapier.com
+	#new will be nh01hq66@robot.zapier.com
 
 	def order_email(order)
 		@order = order
-		mail(to: "l4ddigct@robot.zapier.com", subject: 'Rocketees: New Order')
+		mail(to: "nh01hq66@robot.zapier.com", subject: 'Rocketees: New Order')
 	end
 end

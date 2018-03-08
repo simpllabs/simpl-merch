9426b176d5155516efeaeedeaaf87388e0a32294
Lucas Amorim <amorimluc@gmail.com>
2018-03-08 15:23:51 -0300

    Packing slip controller/view

--- app/mailers/per_order_mailer.rb
+++ app/mailers/per_order_mailer.rb
@@ -1,11 +1,11 @@
 class PerOrderMailer < ApplicationMailer
 	default from: 'amorimluc@gmail.com'
-	#new will be nh01hq66@robot.zapier.com
+	#new will be rocketees.o0vtk8@zapiermail.com
 
 	def order_email(order)
 		@order = order
 		storename = order[5].split('.').first
 		@packing_slip_msg = "https://app.rocketees.com/packingslip/#{storename}"
-		mail(to: "nh01hq66@robot.zapier.com", subject: 'Rocketees: New Order')
+		mail(to: "rocketees.o0vtk8@zapiermail.com", subject: 'Rocketees: New Order')
 	end
 end

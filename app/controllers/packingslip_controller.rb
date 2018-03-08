8c99bf13f3a4f50e70490ef1daf3b144fe833bae
Lucas Amorim <amorimluc@gmail.com>
2018-03-08 13:24:44 -0300

    Packing slip controller/view

--- app/controllers/packingslip_controller.rb
+++ app/controllers/packingslip_controller.rb
@@ -1,6 +1,6 @@
 class PackingslipController < ApplicationController
   def message
   	domain = "#{params[:storename]}.myshopify.com"
-  	@message = Shop.where(shopify_domain: domain).first.packing_slip_message
+  	@message = Shop.where(shopify_domain: domain).first.packing_slip_message if Shop.where(shopify_domain: domain).present?
   end
 end
